1f7f9389fb269a8398a26e937cb0f32be4009df9
Lange Pantoja <wpantoja@paypal.com>
2014-12-03 14:48:48 +0800

    Initial commit.

---
+++ app/src/main/java/com/paypal/hackathon/coupeeds/coupool/CoupoolApplication.java
@@ -0,0 +1,9 @@
+package com.paypal.hackathon.coupeeds.coupool;
+
+import android.app.Application;
+
+/**
+ * Created by wpantoja on 12/3/14.
+ */
+public class CoupoolApplication extends Application {
+}
---
+++ app/src/main/java/com/paypal/hackathon/coupeeds/coupool/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.paypal.hackathon.coupeeds.coupool;
+
+import com.paypal.hackathon.coupeeds.coupool.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class SplashActivity extends BaseActivity {
+
+}
